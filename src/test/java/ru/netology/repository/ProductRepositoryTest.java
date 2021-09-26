29e18dffada0cc3c8493641402b89576ac07da1b
roandr1970 <ambal007a@yandex.ru>
2021-09-26 18:29:49 +0300

    First commit

---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,43 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.manager.ProductManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+
+    Product first = new Book(1, "Windows 11", 500, "Bill");
+    Product second = new Smartphone(2, "samsung", 20000, "Korea");
+    Product third = new Smartphone(3, "samsung", 15000, "Korea");
+
+    @BeforeEach
+    public void setUp() {
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        repository.removeById(2);
+        Product[] actual = repository.findAll();
+        Product[] expected = new Product[]{first, third};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFoundId() {
+        repository.removeById(10);
+        Product[] actual = repository.findAll();
+        Product[] expected = new Product[]{first, second, third};
+        assertArrayEquals(expected, actual);
+    }
+}
